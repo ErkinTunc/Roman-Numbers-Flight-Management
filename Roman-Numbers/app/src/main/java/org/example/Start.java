d0e144c95e80e2690360b76e88bd8fcbc4fc63b0
Nasa Rak <Nasandratriniavo.RAKOTOARISOA@etu.uca.fr>
2026-05-01 19:31:28 +0200

    Ajout des tests dans Start.java

--- Roman-Numbers/app/src/main/java/org/example/Start.java
+++ Roman-Numbers/app/src/main/java/org/example/Start.java
@@ -11,6 +11,31 @@ public class Start {
         System.out.println(roman.getValue());
         System.out.println(roman.getRoman());
 
+	System.out.println("--------------------");
+
+	RomanNumber rom = new RomanNumber("MMXX");
+
+        System.out.println("--------------------");
+
+        System.out.println(rom.getValue());
+        System.out.println(rom.getRoman());
+
+        System.out.println("--------------------");
+
+        System.out.println(RomanConverter.getRomanFromNumber(14)); // XIV
+
+        System.out.println("--------------------");
+
+        System.out.println(RomanConverter.getNumberFromRoman("CD")); // 400
+
+        System.out.println("--------------------");
+
+        try{
+            RomanNumber bad = new RomanNumber("IIII");
+        } catch (Exception e){
+            System.out.println("Error detected " + e.getMessage());
+        }
+
     }
 
 }
