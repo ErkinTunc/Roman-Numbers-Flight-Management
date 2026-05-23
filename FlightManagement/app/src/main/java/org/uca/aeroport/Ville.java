e7adce397299dbf35363cb58bb1c194d60a46dde
ErkinTunc <erkintuncboya@hotmail.com>
2026-05-23 09:05:10 +0200

    Model escales as association class bug fixes

--- FlightManagement/app/src/main/java/org/uca/aeroport/Ville.java
+++ FlightManagement/app/src/main/java/org/uca/aeroport/Ville.java
@@ -1,30 +1,34 @@
 package org.uca.aeroport;
 
-
-public class Ville{
+public class Ville {
 
     private String nom;
 
     // ------------------- Constructors ------------------
 
-    public Ville() { } 
-    
-    public Ville(String nom){
-        this.nom = nom;
+    public Ville() {
+    }
+
+    public Ville(String nom) {
+        setNom(nom);
     }
 
     // ------------------- Getters and Setters ------------------
 
-    public String getNom(){
+    public String getNom() {
         return nom;
     }
-    
-    public void setNom(String nom){
+
+    public void setNom(String nom) {
+        if (nom == null || nom.isBlank()) {
+            throw new IllegalArgumentException("Le nom de la ville est obligatoire");
+        }
+
         this.nom = nom;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return nom;
     }
 
